b446a3e55f380bb739cb7e0d0b94c46ca898313a
ekusiadadus <ekusiadadus@gmail.com>
2021-06-21 05:47:50 +0900

    各手番に戻れるようにする

--- src/index.tsx
+++ src/index.tsx
@@ -169,20 +169,50 @@ const Game: React.FC = () => {
     ]));
     setStepNumber(newHistory.length);
     setXIsNext(!xIsNext);
-  }
+  };
+
+  const jumpTo = (step: number): void => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0)
+  };
+
+
   const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={i => handleClick(i)}/>
+        <Board
+          squares={current.squares}
+          onClick={i => handleClick(i)}
+        />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // ========================================
 
