5cb9b9ec789bbff62db72e6e5ad07f5259129796
ekusiadadus <ekusiadadus@gmail.com>
2021-06-21 05:45:19 +0900

    Winnerを計算する

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,28 @@ import './index.css';
 
 //Squareの値と、型のエイリアス
 type SquareValue = 'X' | 'O' | null;
+
+//Winnerを計算
+const calculateWinner = (squares: SquareValue[]): SquareValue => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 //Propsを、squareprops, boardpropsにする
 interface SquareProps {
   value: SquareValue,
@@ -136,7 +158,7 @@ const Game: React.FC = () => {
     const newHistory = history.slice(0, stepNumber + 1);
     const current = newHistory[newHistory.length - 1];
     const squares = current.squares.slice();
-    if (squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? "X" : "O";
